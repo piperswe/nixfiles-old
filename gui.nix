de3e4dc9fa7740eb7051f96cc7e4aeee281c69af
Piper McCorkle <piper@ladderlife.com>
2020-05-08 13:20:50 -0700

    Alacritty color scheme

--- gui.nix
+++ gui.nix
@@ -43,7 +43,37 @@ in {
     };
   };
 
-  programs.alacritty.enable = true;
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      colors = {
+        primary = {
+          background = "#282828";
+          foreground = "#ebdbb2";
+        };
+        normal = {
+          black = "#282828";
+          red = "#cc241d";
+          green = "#98971a";
+          yellow = "#d7992a";
+          blue = "#458588";
+          magenta = "#b16286";
+          cyan = "#689d6a";
+          white = "#a89984";
+        };
+        bright = {
+          black = "#928374";
+          red = "#fb4934";
+          green = "#b8bb26";
+          yellow = "#fabd2f";
+          blue = "#83a598";
+          magenta = "#d3869b";
+          cyan = "#8ec07c";
+          white = "#ebdbb2";
+        };
+      };
+    };
+  };
 
   programs.rofi = lib.mkIf pkgs.stdenv.isLinux {
     enable = true;
